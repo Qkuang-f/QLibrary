409404e2253c60f28b4e73f27228ca5c3d4ee717
Qkuang <321545536@qq.com>
2021-01-07 14:18:37 +0800

    初始化

---
+++ TestLibray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QkuangLibrary.DataStruct;
+
+namespace TestLibray
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // int[] a = new int[] { 1,2,3,4,5};
+           // int[] b = new int[10];
+           //// a.CopyTo(b, 2);
+           // Console.WriteLine(a.ToString());
+           
+            Console.ReadKey();
+        }
+
+
+    }
+}
